a4d12c39c864a1f064576ac6ff0891f15fcd9bab
bruce1971 <tropsyv3@gmail.com>
2023-07-13 23:13:14 +0200

    refactoring participant

--- participant.js
+++ participant.js
@@ -1,4 +1,3 @@
-const TelegramBot = require('node-telegram-bot-api');
 const axios = require('axios');
 const ETHERSCAN_API_KEY="I2MBIPC3CU5D7WM882FXNFMCHX6FP77IYG";
 const NEXT_PUBLIC_ETHERSCAN_API="https://api.etherscan.io/api";
@@ -8,122 +7,62 @@ let PARTICIPANT_ADDRESS = '0x70399b85054dd1d94f2264afc8704a3ee308abaf' //scribbs
 PARTICIPANT_ADDRESS = PARTICIPANT_ADDRESS.toLowerCase();
 
 
-async function getEtherscanData() {
-  const baseUrl = NEXT_PUBLIC_ETHERSCAN_API;
-  const API_KEY = ETHERSCAN_API_KEY;
-  const url = `
+function accountUrl(type) {
+  return `
     https://api.etherscan.io/api
      ?module=account
-     &action=txlist
+     &action=${type}
      &address=${PARTICIPANT_ADDRESS}
      &startblock=0
      &endblock=99999999
      &page=1
      &sort=desc
      &apikey=${ETHERSCAN_API_KEY}
-  `.replace(/\s/g, '');
-  axios
-   .get(url)
-   .then(function (response) {
-     let value;
-     let ins = 0;
-     let outs = 0;
-     const normalTransactions = response.data.result;
-     normalTransactions.forEach(tx => {
-       console.log('-----------------');
-       if (tx.from.toLowerCase() === PARTICIPANT_ADDRESS && tx.functionName === '' && tx.input === '0x') {
-         value = Number(tx.value)/10**18;
-         outs = outs + value;
-         console.log(`Send ${value}eth to ${tx.to}`);
-       } else if (tx.to.toLowerCase() === PARTICIPANT_ADDRESS && tx.functionName === '') {
-         value = Number(tx.value)/10**18;
-         ins = ins + value;
-         console.log(`Receive ${value}eth from ${tx.from}`);
-       } else if (tx.to.toLowerCase() === '0x283Af0B28c62C092C9727F1Ee09c02CA627EB7F5'.toLowerCase() && tx.functionName.includes('commit')) {
-         console.log(`Request to Register ENS Domain`);
-       } else if (tx.to.toLowerCase() === '0x283Af0B28c62C092C9727F1Ee09c02CA627EB7F5'.toLowerCase() && tx.functionName.includes('registerWithConfig')) {
-         console.log(`Register ENS Domain`);
-       } else {
-         console.log(tx);
-       }
-     });
-     // console.log('INS',ins);
-     // console.log('OUTS',outs);
-     // const profit = outs - ins;
-     // console.log('PROFIT', profit);
-   });
+  `.replace(/\s/g, '')
+}
 
-  // const url2 = `
-  //   https://api.etherscan.io/api
-  //    ?module=account
-  //    &action=txlistinternal
-  //    &address=${PARTICIPANT_ADDRESS}
-  //    &startblock=0
-  //    &endblock=99999999
-  //    &page=1
-  //    &sort=desc
-  //    &apikey=${ETHERSCAN_API_KEY}
-  // `.replace(/\s/g, '');
-  // axios
-  //  .get(url2)
-  //  .then(function (response) {
-  //    const internalTransactions = response.data.result;
-  //    console.log('int',internalTransactions.length);
-  //  });
-  //
-  // const url3 = `
-  //   https://api.etherscan.io/api
-  //    ?module=account
-  //    &action=tokentx
-  //    &address=${PARTICIPANT_ADDRESS}
-  //    &startblock=0
-  //    &endblock=99999999
-  //    &page=1
-  //    &sort=desc
-  //    &apikey=${ETHERSCAN_API_KEY}
-  // `.replace(/\s/g, '');
-  // axios
-  //  .get(url3)
-  //  .then(function (response) {
-  //    const erc20Transactions = response.data.result;
-  //    console.log('erc20',erc20Transactions.length);
-  //  });
-  //
-  // const url4 = `
-  //   https://api.etherscan.io/api
-  //    ?module=account
-  //    &action=tokennfttx
-  //    &address=${PARTICIPANT_ADDRESS}
-  //    &startblock=0
-  //    &endblock=99999999
-  //    &page=1
-  //    &sort=desc
-  //    &apikey=${ETHERSCAN_API_KEY}
-  // `.replace(/\s/g, '');
-  // axios
-  //  .get(url4)
-  //  .then(function (response) {
-  //    const erc721Transactions = response.data.result;
-  //    console.log('erc721',erc721Transactions.length);
-  //  });
-  //
-  // const url5 = `
-  //   https://api.etherscan.io/api
-  //    ?module=account
-  //    &action=token1155tx
-  //    &address=${PARTICIPANT_ADDRESS}
-  //    &startblock=0
-  //    &endblock=99999999
-  //    &page=1
-  //    &sort=desc
-  //    &apikey=${ETHERSCAN_API_KEY}
-  // `.replace(/\s/g, '');
-  // axios
-  //  .get(url5)
-  //  .then(function (response) {
-  //    const erc1155Transactions = response.data.result;
-  //    console.log('erc1155',erc1155Transactions.length);
-  //  });
+
+async function getEtherscanData() {
+
+  const normalTransactions = await axios.get(accountUrl('txlist')).then(res => res.data.result);
+  console.log(normalTransactions.length);
+  const internalTransactions = await axios.get(accountUrl('txlistinternal')).then(res => res.data.result);
+  console.log(internalTransactions.length);
+  const erc20Transactions = await axios.get(accountUrl('tokentx')).then(res => res.data.result);
+  console.log(erc20Transactions.length);
+  const erc721Transactions = await axios.get(accountUrl('tokennfttx')).then(res => res.data.result);
+  console.log(erc721Transactions.length);
+  const erc1155Transactions = await axios.get(accountUrl('token1155tx')).then(res => res.data.result);
+  console.log(erc1155Transactions.length);
+
+  let value, ins = 0, outs = 0;
+  normalTransactions.forEach(tx => {
+    console.log('-----------------');
+    if (tx.from.toLowerCase() === PARTICIPANT_ADDRESS && tx.functionName === '' && tx.input === '0x') {
+      value = Number(tx.value)/10**18;
+      outs = outs + value;
+      console.log(`Send ${value}eth to ${tx.to}`);
+    } else if (tx.to.toLowerCase() === PARTICIPANT_ADDRESS && tx.functionName === '') {
+      value = Number(tx.value)/10**18;
+      ins = ins + value;
+      console.log(`Receive ${value}eth from ${tx.from}`);
+    } else if (tx.to.toLowerCase() === '0x283Af0B28c62C092C9727F1Ee09c02CA627EB7F5'.toLowerCase() && tx.functionName.includes('commit')) {
+      console.log(`Request to Register ENS Domain`);
+    } else if (tx.to.toLowerCase() === '0x283Af0B28c62C092C9727F1Ee09c02CA627EB7F5'.toLowerCase() && tx.functionName.includes('registerWithConfig')) {
+      console.log(`Register ENS Domain`);
+    } else if (tx.from.toLowerCase() === PARTICIPANT_ADDRESS && tx.functionName === '') {
+      value = Number(tx.value)/10**18;
+      console.log(`Likely a ${value}eth NFT buy...`);
+      console.log(`https://etherscan.io/tx/${tx.hash}`);
+    } else {
+      console.log(tx);
+    }
+  });
+  console.log('INS',ins);
+  console.log('OUTS',outs);
+  const profit = outs - ins;
+  console.log('PROFIT', profit);
+
 }
 
 getEtherscanData()
