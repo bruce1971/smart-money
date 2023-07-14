eaf5b5c49be65862b595f892aa4791644b057a03
bruce1971 <tropsyv3@gmail.com>
2023-07-14 17:38:16 +0200

    more cleaning

--- participant.js
+++ participant.js
@@ -22,46 +22,87 @@ function accountUrl(type) {
 }
 
 
+function parseTx(tx) {
+  value = Number(tx.value)/10**18;
+  if (tx.from.toLowerCase() === PARTICIPANT_ADDRESS && tx.functionName === '' && tx.input === '0x') {
+    console.log(`Send ${value}eth to ${tx.to}`);
+  } else if (tx.to.toLowerCase() === PARTICIPANT_ADDRESS && tx.functionName === '') {
+    console.log(`Receive ${value}eth from ${tx.from}`);
+  } else if (tx.to.toLowerCase() === '0x283Af0B28c62C092C9727F1Ee09c02CA627EB7F5'.toLowerCase() && tx.functionName.includes('commit')) {
+    console.log(`Request to Register ENS Domain`);
+  } else if (tx.to.toLowerCase() === '0x283Af0B28c62C092C9727F1Ee09c02CA627EB7F5'.toLowerCase() && tx.functionName.includes('registerWithConfig')) {
+    console.log(`Register ENS Domain`);
+  } else if (tx.from.toLowerCase() === PARTICIPANT_ADDRESS && tx.functionName === '') {
+    console.log(`NFT buy! Bought ${tx.tokenName} ${tx.tokenID} for ${value}eth`);
+  } else if (tx.functionName === 'withdraw(uint256 amount)' && tx.to.toLowerCase() == '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2'.toLowerCase()) {
+    console.log(`withdraw(uint256 amount)`);
+    console.log(tx);
+    console.log(`Unwrap WETH to ETH`); //amount in decoded tx.input
+  } else if (tx.functionName === 'withdraw(uint256 amount)' && tx.to.toLowerCase() == '0x0000000000a39bb272e79075ade125fd351887ac'.toLowerCase()) {
+    console.log(`withdraw(uint256 amount)`);
+    console.log(tx);
+    console.log(`Withdraw from Blur`);
+  } else {
+    // console.log(tx);
+  }
+}
+
+
 async function getEtherscanData() {
 
-  const normalTransactions = await axios.get(accountUrl('txlist')).then(res => res.data.result);
-  console.log(normalTransactions.length);
-  const internalTransactions = await axios.get(accountUrl('txlistinternal')).then(res => res.data.result);
-  console.log(internalTransactions.length);
-  const erc20Transactions = await axios.get(accountUrl('tokentx')).then(res => res.data.result);
-  console.log(erc20Transactions.length);
-  const erc721Transactions = await axios.get(accountUrl('tokennfttx')).then(res => res.data.result);
-  console.log(erc721Transactions.length);
-  const erc1155Transactions = await axios.get(accountUrl('token1155tx')).then(res => res.data.result);
-  console.log(erc1155Transactions.length);
-
-  let value, ins = 0, outs = 0;
-  normalTransactions.forEach(tx => {
-    console.log('-----------------');
-    if (tx.from.toLowerCase() === PARTICIPANT_ADDRESS && tx.functionName === '' && tx.input === '0x') {
-      value = Number(tx.value)/10**18;
-      outs = outs + value;
-      console.log(`Send ${value}eth to ${tx.to}`);
-    } else if (tx.to.toLowerCase() === PARTICIPANT_ADDRESS && tx.functionName === '') {
-      value = Number(tx.value)/10**18;
-      ins = ins + value;
-      console.log(`Receive ${value}eth from ${tx.from}`);
-    } else if (tx.to.toLowerCase() === '0x283Af0B28c62C092C9727F1Ee09c02CA627EB7F5'.toLowerCase() && tx.functionName.includes('commit')) {
-      console.log(`Request to Register ENS Domain`);
-    } else if (tx.to.toLowerCase() === '0x283Af0B28c62C092C9727F1Ee09c02CA627EB7F5'.toLowerCase() && tx.functionName.includes('registerWithConfig')) {
-      console.log(`Register ENS Domain`);
-    } else if (tx.from.toLowerCase() === PARTICIPANT_ADDRESS && tx.functionName === '') {
-      value = Number(tx.value)/10**18;
-      console.log(`Likely a ${value}eth NFT buy...`);
-      console.log(`https://etherscan.io/tx/${tx.hash}`);
-    } else {
-      console.log(tx);
-    }
+  const normalTransactions = await axios.get(accountUrl('txlist')).then(res => {
+    const txs = res.data.result;
+    txs.forEach(tx => tx.type = 'normal');
+    return txs;
   });
-  console.log('INS',ins);
-  console.log('OUTS',outs);
-  const profit = outs - ins;
-  console.log('PROFIT', profit);
+  const internalTransactions = await axios.get(accountUrl('txlistinternal')).then(res => {
+    const txs = res.data.result;
+    txs.forEach(tx => tx.type = 'internal')
+    return txs;
+  });
+  const erc20Transactions = await axios.get(accountUrl('tokentx')).then(res => {
+    const txs = res.data.result;
+    txs.forEach(tx => tx.type = 'erc20')
+    return txs;
+  });
+  const erc721Transactions = await axios.get(accountUrl('tokennfttx')).then(res => {
+    const txs = res.data.result;
+    txs.forEach(tx => tx.type = 'erc721')
+    return txs;
+  });
+  const erc1155Transactions = await axios.get(accountUrl('token1155tx')).then(res => {
+    const txs = res.data.result;
+    txs.forEach(tx => tx.type = 'erc1155')
+    return txs;
+  });
+
+  let transactions = [
+    ...normalTransactions,
+    ...internalTransactions,
+    ...erc20Transactions,
+    ...erc721Transactions,
+    ...erc1155Transactions
+  ];
+
+  let txHashes = [...new Set(transactions.map(tx => tx.hash))];
+  let txArray = [];
+  txHashes.forEach(hash => {
+    const txs = transactions.filter(tx => tx.hash === hash);
+    const timeStamp = Number(txs[0].timeStamp);
+    txArray.push({
+      hash: hash,
+      timeStamp: timeStamp,
+      txs: txs
+    })
+  });
+  txArray = txArray.sort((a, b) => Number(b.timeStamp) - Number(a.timeStamp))
+
+  txArray.forEach(tx => {
+    console.log('-----------------');
+    console.log(`https://etherscan.io/tx/${tx.hash}`);
+    console.log(tx);
+    // parseTx(tx)
+  });
 
 }
 
