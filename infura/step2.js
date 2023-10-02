62200e83023bd96e2bf8df2d00c931dfe896df64
bruce1971 <tropsyv3@gmail.com>
2023-10-02 12:20:31 +0200

    refactor: data fetch speed up

--- infura/step2.js
+++ infura/step2.js
@@ -12,110 +12,125 @@ var provider = `https://mainnet.infura.io/v3/${APIKEY}`;
 var web3Provider = new Web3.providers.HttpProvider(provider);
 var web3 = new Web3(web3Provider);
 const ObjectsToCsv = require('objects-to-csv');
-let allUserAddresses = [];
 
 
-async function getTxsData(contractObject, fromBlock, toBlock) {
-  let erc20Transactions = await axios.get(`
-    https://api.etherscan.io/api
-     ?module=account
-     &action=tokentx
-     &contractaddress=${contractObject.contractAddress}
-     &startblock=${fromBlock}
-     &endblock=${toBlock}
-     &sort=asc
-     &apikey=${etherscanApiKey}
-  `.replace(/\s/g, '')).then(res => {
-    const txs = res.data.result;
-    const final = [];
-    txs.forEach(tx => final.push({
-      blockNumber: tx.blockNumber,
-      timeStamp: tx.timeStamp,
-      hash: tx.hash,
-      from: tx.from,
-      contractAddress: tx.contractAddress,
-      to: tx.to,
-      value: tx.value,
-      tokenName: tx.tokenName,
-      tokenSymbol: tx.tokenSymbol,
-      tokenDecimal: tx.tokenDecimal
-    }))
-    return final;
-  });
-  const toAddresses = [... new Set(erc20Transactions.map(tx => tx.to))];
-  const fromAddresses = [... new Set(erc20Transactions.map(tx => tx.from))];
-  let userAddresses = [...toAddresses, ...fromAddresses];
-  userAddresses = userAddresses.filter(a => a !== contractObject.pairAddress);
-
-  const prevAllUserCount = allUserAddresses.length;
-  allUserAddresses = [... new Set([...allUserAddresses, ...userAddresses])];
-  const newUserCount = allUserAddresses.length - prevAllUserCount;
-
-  if (userAddresses.includes('0x70399b85054dd1d94f2264afc8704a3ee308abaf')) {
-    console.log('SCRIIIIIIIIIIIIIIIIIIBS');
+async function getData(contractObject, startBlock, endBlock) {
+  let allData = [];
+  const blockIncr = 1000;
+  let currentBlock = startBlock;
+  while (currentBlock < endBlock) {
+    console.log('====');
+    console.log(`Blocks remaining: ${endBlock-currentBlock} (~${Math.ceil((endBlock-currentBlock)/blockIncr)} loops)`);
+    let erc20Transactions = await axios.get(`
+      https://api.etherscan.io/api
+       ?module=account
+       &action=tokentx
+       &contractaddress=${contractObject.contractAddress}
+       &startblock=${currentBlock}
+       &endblock=${currentBlock+blockIncr}
+       &sort=asc
+       &apikey=${etherscanApiKey}
+    `.replace(/\s/g, '')).then(res => {
+      const txs = res.data.result;
+      const final = [];
+      txs.forEach(tx => final.push({
+        blockNumber: tx.blockNumber,
+        timeStamp: tx.timeStamp,
+        hash: tx.hash,
+        from: tx.from,
+        contractAddress: tx.contractAddress,
+        to: tx.to,
+        value: tx.value,
+        tokenName: tx.tokenName,
+        tokenSymbol: tx.tokenSymbol,
+        tokenDecimal: tx.tokenDecimal
+      }))
+      return final;
+    });
+    console.log(`Tx count: ${erc20Transactions.length}`);
+    if (erc20Transactions.length >= 10000) console.log('WARNING: reached 10000. Fix now!');
+    allData = allData.concat(erc20Transactions);
+    // while loop incr
+    currentBlock = currentBlock + blockIncr + 1;
   }
-
-  let buys = 0;
-  let sells = 0;
-  erc20Transactions.forEach(o => {
-    if (o.to === contractObject.pairAddress) sells += 1;
-    if (o.from === contractObject.pairAddress) buys += 1;
-  });
-  return {
-    txCount: erc20Transactions.length,
-    userCount: userAddresses.length,
-    userCountAcc: allUserAddresses.length,
-    newUserCount: newUserCount,
-    buys: buys,
-    sells: sells
-  }
+  return allData;
 }
 
 
-async function save(db2, contractObject, caDb2, name, minIncr, nLoops) {
-  db2[contractObject.contractAddress] = caDb2;
+function aggrData(data, contractObject) {
+  const finalData = [];
+  let allUserAddresses = [];
+  const aggrSize = 5; //blocks
+  let currentBlock = Number(data[0].blockNumber);
+  const endBlock = Number(data[data.length - 1].blockNumber);
+  while (currentBlock < (endBlock - 1*aggrSize)) {
+    // filter on subset of data
+    const subData = data.filter(o => currentBlock <= Number(o.blockNumber) && Number(o.blockNumber) <= currentBlock + aggrSize);
+
+    // compute newUserCount
+    const toAddresses = [... new Set(subData.map(tx => tx.to))];
+    const fromAddresses = [... new Set(subData.map(tx => tx.from))];
+    let userAddresses = [...toAddresses, ...fromAddresses];
+    userAddresses = userAddresses.filter(a => a !== contractObject.pairAddress);
+    const prevAllUserCount = allUserAddresses.length; //???
+    allUserAddresses = [... new Set([...allUserAddresses, ...userAddresses])];
+    const newUserCount = allUserAddresses.length - prevAllUserCount;
+
+    // compute buys & sells
+    let buys = 0, sells = 0;
+    subData.forEach(o => {
+      if (o.to === contractObject.pairAddress) sells += 1;
+      if (o.from === contractObject.pairAddress) buys += 1;
+    });
+
+    // append to collector
+    finalData.push({
+      startBlock: currentBlock,
+      endblock: currentBlock + aggrSize,
+      txCount: subData.length,
+      userCount: userAddresses.length,
+      newUserCount: newUserCount,
+      buys: buys,
+      sells: sells
+    });
+
+    // while loop incr
+    currentBlock = currentBlock + aggrSize + 1;
+  }
+  return finalData;
+}
+
+
+async function saveData(finalData, db2, contractObject, name) {
+  db2[contractObject.contractAddress] = finalData;
   await fs.writeFile(path_db2, JSON.stringify(db2, null, 2), 'utf8');
-  const csv = new ObjectsToCsv(caDb2);
+  const csv = new ObjectsToCsv(finalData);
   await csv.toDisk(`./infura/data/csv/${name}.csv`);
 }
 
 
 async function intervalExecute(contractObject, name) {
+  // 1) Determine blockrange of interest
   const db2 = JSON.parse(await fs.readFile(path_db2));
-  let caDb2, iStart;
-  if (db2[contractObject.contractAddress]) {
+  let caDb2, startBlock;
+  // if (db2[contractObject.contractAddress]) { // DOESNT WORK BECAUSE OF ALLUSERADDRESSES RESET
+  if (false) {
     caDb2 = db2[contractObject.contractAddress];
-    iStart = caDb2.length;
+    startBlock = caDb2[caDb2.length - 1].endblock + 1;
   } else {
     caDb2 = [];
-    iStart = 0;
+    startBlock = contractObject.blockNumber;
   }
-  let startBlock = contractObject.blockNumber;
-  const nLoops = 60 * 24 * 7 * 3;
-  const minIncr = 1;
-  const blockIncr = 5 * minIncr;
-  for (let i = iStart; i < nLoops; i++) {
-    console.log('==========================================================================');
-    console.log(`min ${minIncr*(i+1)}/${minIncr*nLoops}`);
-    let txData = await getTxsData(
-      contractObject,
-      startBlock,
-      startBlock + blockIncr,
-    );
-    txData = {
-      min: minIncr*(i+1),
-      startBlock: startBlock,
-      endblock: startBlock + blockIncr,
-      minIncr: minIncr,
-      ...txData
-    }
-    console.log(txData);
-    caDb2.push(txData);
-    startBlock = startBlock + blockIncr + 1;
-    if (i % 100 === 0) await save(db2, contractObject, caDb2, name, minIncr, nLoops);
-  }
+  const endBlock = startBlock + 10080; // next 10k blocks
 
-  await save(db2, contractObject, caDb2, name, minIncr, nLoops);
+  // 2) Get data for blockrange of interest
+  const allData = await getData(contractObject, startBlock, endBlock);
+
+  // 3) Group & aggr in buckets of 5 blocks
+  const finalData = aggrData(allData, contractObject);
+
+  // 4) Save data
+  await saveData(finalData, db2, contractObject, name);
 }
 
 
@@ -125,8 +140,11 @@ if (require.main === module) {
     // const name = "Pepe";
     // const name = "CUCK";
     // const name = "NiHao";
-    const name = "NicCageWaluigiElmo42069Inu";
+    // const name = "NicCageWaluigiElmo42069Inu";
+    const name = "AstroPepeX";
     const contractObject = Object.values(db1).find(o => o.name === name);
-    intervalExecute(contractObject, name);
+    console.time('Timer');
+    await intervalExecute(contractObject, name);
+    console.timeEnd('Timer');
   })();
 }
